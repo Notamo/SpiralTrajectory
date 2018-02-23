9d02c5a08fc39c0d723d9a02702c928fa01cc426
Joey <Joey@10.0.0.195>
2018-02-22 19:22:56 -0800

    Platform file

---
+++ SpiralTrajectoryGame/public_html/src/MyGame/Objects/Platform.js
@@ -0,0 +1,29 @@
+/* File: Platform.js 
+ *
+ * Creates and initializes the Hero (Dye)
+ * overrides the update function of GameObject to define
+ * simple Dye behavior
+ */
+
+/*jslint node: true, vars: true */
+/*global gEngine, GameObject, SpriteRenderable, vec2, RigidShape, RigidRectangle */
+/* find out more about jslint: http://www.jslint.com/help.html */
+
+"use strict";
+
+function Platform(spriteTexture, x, y, w, h) {
+    this.platform = new TextureRenderable(spriteTexture);
+    var xform = this.platform.getXform();
+    xform.setSize(w, h);
+    xform.setPosition(x, y);
+    GameObject.call(this, this.platform);
+    
+    var r = new RigidRectangle(xform, w, h);
+    r.setMass(0);
+    this.setRigidBody(r);
+    
+}
+gEngine.Core.inheritPrototype(Platform, GameObject);
+
+Platform.prototype.update = function () {
+};
