053be1a6e6e5dda30fa2d7e2868fc6d6f59609e3
Justin Baker <JustinRBaker88@gmail.com>
2018-02-28 16:10:26 -0800

    Updated ArrowSet. Has support for cooldowns and max arrow count.

--- SpiralTrajectoryGame/public_html/src/MyGame/Objects/Arrows/ArrowSet.js
+++ SpiralTrajectoryGame/public_html/src/MyGame/Objects/Arrows/ArrowSet.js
@@ -1,17 +1,104 @@
+/* File: ArrowSet.js 
+ *
+ * Support for working with a set of Arrows
+ */
 class ArrowSet extends GameObjectSet {
+    
+    /**
+    * Default Constructor
+    * Support for working with a set of Arrows, including Ice & Fire Arrows
+    * @returns {ArrowSet} New instance of ArrowSet
+    * @class ArrowSet
+    */
     constructor(){
         this.super();
         
         // { Regular : 0, Fire : 1, Ice : 2 }
         // Cooldowns (frames) for each type of arrow
-        this.kCooldowns = [30, 600, 300];
+        this.kCooldowns = [30, 300, 300];
+        this.mTimeSinceSpawn = [0, 0, 0];
         // max count of each arrow type
-        this.kMaxCount = [10, 4, 2];
+        this.kMaxCount = [10, 4, 4];
         
-        this.mRegArrows = [];
-        this.mFireArrows = [];
-        this.mIceArrows = [];
+        this.mRegArrows = new GameObjectSet();
+        this.mFireArrows = new GameObjectSet();
+        this.mIceArrows = new GameObjectSet();
+    
     }
     
+    /**
+    * Add Arrow to this ArrowSet and a subset for specific arrow type
+    * @param {GameObject} obj to add to this GameObjectSet
+    * @returns {void}
+    * @memberOf ArrowSet
+    */
+    addToSet(obj) {
+        if (obj instanceof Arrow){
+            var success = false;
+            if (obj instanceof FireArrow) {
+                if (this.mFireArrows.size() < this.kMaxCount[1] && this.mTimeSinceSpawn[1] < this.kCooldowns[1]) {
+                    this.mFireArrows.addToSet(obj);
+                    this.mTimeSinceSpawn[1] = 0;
+                    success = true;
+                }
+            }
+            else if (obj instanceof IceArrow) {
+                if (this.mIceArrows.size() < this.kMaxCount[2] && this.mTimeSinceSpawn[2] < this.kCooldowns[2]) {
+                    this.mIceArrows.addToSet(obj);
+                    this.mTimeSinceSpawn[2] = 0;
+                    success = true;
+                }
+            }
+            else {
+                if (this.mRegArrows.size() < this.kMaxCount[0] && this.mTimeSinceSpawn[0] < this.kCooldowns[0]) {
+                    this.mIceArrows.addToSet(obj);
+                    this.mTimeSinceSpawn[0] = 0;
+                    success = true;
+                }
+            }
+            if (this.size() < this.kMaxCount.reduce((accumulator, currentValue) => accumulator + currentValue) && success) {
+                super.addToSet(obj);
+            }
+        }
+    }
     
+    /**
+    * Return the count of Arrows in set
+    * @returns {Number} count of Arrows in set
+    * @memberOf ArrowSet
+    */
+    size() {
+        var sum = 0;
+        sum += this.mRegArrows.size();
+        sum += this.mFireArrows.size();
+        sum += this.mIceArrows.size();
+        return sum;
+    }
+    
+    /**
+    * Remove Arrow from ArrowSet & subsets
+    * @param {GameObject} obj to remove from GameObjectSet
+    * @returns {void}
+    * @memberOf GameObjectSet
+    */
+    removeFromSet(obj) {
+        this.mRegArrows.removeFromSet(obj);
+        this.mFireArrows.removeFromSet(obj);
+        this.mIceArrows.removeFromSet(obj);
+        super.removeFromSet(obj);
+    }
+    
+    /**
+    * Update function called by GameLoop calls all Arrows's in ArrowSet
+    * @returns {void}
+    * @memberOf ArrowSet
+    */
+    update() {
+        super.update();
+        var i;
+        for (i = 0; i < this.mTimeSinceSpawn; i++) {
+            this.mTimeSinceSpawn[i]++;
+        }
+        //TODO: Check arrows for expired state to remove them.
+    }
 }
