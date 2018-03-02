d768b4281f04a592d29b6b1eef2a57db670da579
Joey <Joey@10.0.0.195>
2018-03-01 21:21:21 -0800

    Config system implemented.

---
+++ SpiralTrajectoryGame/public_html/src/MyGame/Config/Config_BossBattle.js
@@ -0,0 +1,33 @@
+/* File: Config_BossBattle.js 
+ */
+
+/*jslint node: true, vars: true */
+/*global gEngine, Config, BossBattle, vec2*/
+/* find out more about jslint: http://www.jslint.com/help.html */
+
+"use strict";
+
+var Config = Config || {};
+
+Config.BossBattle = Object.freeze({
+    Textures: {
+        HeroSprite:             "assets/characters/hero.png",
+        DefaultArrowSprite:     "assets/projectiles/arrow.png",
+        IceArrowSprite:         "assets/projectiles/icearrow.png",
+        FireArrowSprite:        "assets/projectiles/firearrow.png",
+        BossSprite:             "assets/characters/boss_sprites.png",
+        PlatformTexture:        "assets/props/platform.png",
+        GroundTexture:          "assets/props/platform.png",
+        WallTexture:            "assets/wall.png",
+        TorchTexture:           "assets/props/torch1.png",
+        TorchParticleTexture:   "assets/particle.png"
+    },
+    Cameras: {
+        MainCameraStartingPosition:     vec2.fromValues(25, 1),
+        MainCameraWorldWidth:           200,
+        MainCameraViewport:             [0, 0, 1200, 900],
+        MainCameraBackgroundColor:      [0.8, 0.8, 0.8, 1],
+        MainCameraInterpStiffness:      0.2,
+        MainCameraInterpDuration:       30
+    }
+});
---
+++ SpiralTrajectoryGame/public_html/src/MyGame/Config/Config_Engine.js
@@ -0,0 +1,18 @@
+/* File: Config_BossBattle.js 
+ */
+
+/*jslint node: true, vars: true */
+/*global gEngine, Config, BossBattle, vec2*/
+/* find out more about jslint: http://www.jslint.com/help.html */
+
+"use strict";
+
+var Config = Config || {};
+
+Config.Engine = Object.freeze({
+    Misc: {
+        CanvasClearColor:               [0.8, 0.8, 0.8, 1],
+        GlobalAmbientIntensity:         2.5,
+        GlobalAmbientColor:             [0.3, 0.325, 0.3, 0.4]
+    }
+});
