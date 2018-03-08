4a8975adc01042ef9a34cd96973ea29b58d413d7
Justin Baker <JustinRBaker88@gmail.com>
2018-03-07 16:11:32 -0800

    Changing volume values.

--- SpiralTrajectoryGame/public_html/src/MyGame/Scenes/BossBattle/BossBattle.js
+++ SpiralTrajectoryGame/public_html/src/MyGame/Scenes/BossBattle/BossBattle.js
@@ -85,7 +85,7 @@ BossBattle.prototype.initialize = function () {
     this.mPhysicsGameObjects.addToSet(this.mBoss);
     
     this.kBgMusic = "assets/audio/music/bossbattle.mp3";
-    gEngine.AudioClips.playBackgroundAudio(this.kBgMusic);
+    gEngine.AudioClips.playBackgroundAudio(this.kBgMusic, .05);
     
     this.buildLevel();
     
